81162c771e657c50a46896a64e2e011a11f4ecc1
wenliang peng <wenliang.peng@tum.de>
2021-11-23 19:19:02 +0100

    add subdata copy function

---
+++ data_subset_semantic_kitti.py
@@ -0,0 +1,37 @@
+import random
+import shutil
+import os
+# total num for train 18840
+# total num for val 4350
+NUM_TRAIN = int(1884/2)
+NUM_VAL = int(435/2)
+
+def split_subdataset():
+    lines_train = open(
+        r'configs\datasets\semantic_kitti\train.txt').read().splitlines()
+    with open('configs\datasets\semantic_kitti\sub_train_{NUM_TRAIN}.txt'.format(NUM_TRAIN=NUM_TRAIN), 'w') as f:
+        for i in range(NUM_TRAIN):
+            train_line = random.choice(lines_train)
+            f.write(train_line)
+            f.write('\n')
+
+    lines_val = open(
+        r'configs\datasets\semantic_kitti\val.txt').read().splitlines()
+    with open('configs\datasets\semantic_kitti\sub_val_{NUM_VAL}.txt'.format(NUM_VAL=NUM_VAL), 'w') as f:
+        for i in range(NUM_VAL):
+            val_line = random.choice(lines_val)
+            f.write(val_line)
+            f.write('\n')
+
+def copy_subdataset():
+    input_root="./data/semantic_kitti/"
+    target_root="../sub_dataset/"
+    lines_train = open(
+        r'configs\datasets\semantic_kitti\sub_train_{NUM_TRAIN}.txt'.format(NUM_TRAIN=NUM_TRAIN)).read().splitlines()
+    for line in lines_train:
+        input_file_path=os.path.join(input_root,line)
+        ouput_file_path=os.path.join(target_root,line)
+        os.makedirs(os.path.dirname(ouput_file_path), exist_ok=True)
+        shutil.copy(input_file_path, ouput_file_path)
+
+copy_subdataset()
